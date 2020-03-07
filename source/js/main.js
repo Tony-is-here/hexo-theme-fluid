7f855d70032974a75f523154fd90c02f76f8f0a9
zkqiang <zkqiang@126.com>
2020-03-07 17:07:37 +0800

    :art: 优化视差滚动

--- source/js/main.js
+++ source/js/main.js
@@ -13,9 +13,9 @@ function scrollToBoard() {
 // 防抖动函数
 function debounce(func, wait, immediate) {
   var timeout;
-  return function() {
+  return function () {
     var context = this, args = arguments;
-    var later = function() {
+    var later = function () {
       timeout = null;
       if (!immediate) func.apply(context, args);
     };
@@ -46,16 +46,23 @@ $(document).ready(function () {
     $('#navbar').toggleClass('navbar-col-show');
   });
 
-  // 头图滚动动画
-  $(window).scroll(function () {
+  // 头图视差动画
+  var parallax = function () {
     var oVal = $(window).scrollTop() / 3;
+    var offset = parseInt($('#board').css('margin-top'));
+    var max = 144 + offset;
+    if (oVal > max) {
+      oVal = max;
+    }
     $('#background[parallax="true"]').css({
       transform: 'translate3d(0,' + oVal + 'px,0)',
       '-webkit-transform': 'translate3d(0,' + oVal + 'px,0)',
       '-ms-transform': 'translate3d(0,' + oVal + 'px,0)',
       '-o-transform': 'translate3d(0,' + oVal + 'px,0)',
     });
-  });
+  };
+  parallax();
+  $(window).scroll(parallax);
 
   // 向下滚动箭头的点击
   $('.scroll-down-bar').on('click', scrollToBoard);
